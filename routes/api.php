517f0836fe481c68418bff801337df41dc04ba93
bloodykheeng <69955065+bloodykheeng@users.noreply.github.com>
2024-08-06 11:05:50 +0300

    test

--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token, Authorization, Accept,charset,boundary,Content-Length');
 
-// Public Routes
+/// Public Routes
 Route::post('/register', [AuthController::class, 'register']);
 // Route::post('/login', [AuthController::class, 'login']);
 
