4106136dc8ed58d496db740e48a17e987c07143f
Kalema Mark <kalemamark46@gmail.com>
2024-08-16 12:41:03 +0300

    random string

--- app/Http/Controllers/API/OrderController.php
+++ app/Http/Controllers/API/OrderController.php
@@ -58,12 +58,12 @@ class OrderController extends Controller
             // Start a database transaction
             DB::beginTransaction();
 
-            // do {
-            //     $orderNumber = strtoupper(Str::random(10));
-            // } while (Order::where('order_number', $orderNumber)->exists());
+            do {
+                $orderNumber = strtoupper(Str::random(10));
+            } while (Order::where('order_number', $orderNumber)->exists());
             
-            $orderNumber = strtoupper(Str::uuid()->toString());
-            
+            // $orderNumber = strtoupper(Str::uuid()->toString());
+
             // Create the Order
             $order = Order::create([
                 'status' => 'active',
